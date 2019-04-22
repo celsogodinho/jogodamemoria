653df4373aa7f982b0fd363071ecb5cc88740b42
Celso Godinho da Costa <celsogod@gmail.com>
2019-04-22 20:38:03 -0300

    Implementação da lógica do jogo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,63 @@
+// Objeto que armazena as informações de cada cartão, indexado pelo id do cartã0
+var tabuleiro = {};
+// informa quantas imagens já foram descobertas
+var descobertas = 0;
+// armazena o id do primeiro cartão virado
+var virado = '';
+// Caminho do verso do cartão
+var img_verso = "img/verso.jpg";
+// Array com as imagens que podem ser usadas no jogo
+var imgs_cartoes = [
+    "cachorro.jpg", "cavalo.jpg", "coala.jpg",  "esquilo.jpg", 
+    "pato.jpg", "preguica.jpg",  "tigre.jpg",  "vaca.jpg",  
+    "cachorro.jpg", "cavalo.jpg", "coala.jpg", "esquilo.jpg", 
+    "pato.jpg", "preguica.jpg",  "tigre.jpg",  "vaca.jpg",              
+];
+
+// Função que executa a lógica do jogo
+function jogoMemoria(){
+    let id = this.id;
+    let cartao = tabuleiro[id];
+
+    if (virado == ''){ // Não há nenhum cartão virado
+        // guarda o id do cartão clicado
+        virado = id;
+        // mostra a imagem do cartão
+        this.src = cartao.imagem; 
+    } else { // Há um cartão virado
+        // Verifica se o cartão anteriormente virado é o clicado agora
+        if(virado == id){
+            // se foi o mesmo cartão clicado
+            // mostramos a imagem de verso e resetamos o virado
+            this.src = img_verso;
+            virado = '';
+        } else { // Um outro cartão foi clicado
+            // mostramos a imagem
+            this.src = cartao.imagem; 
+
+            // os dois cartões armazenam a mesma imagem
+            if (tabuleiro[id].imagem == tabuleiro[virado].imagem){
+                // incrementamos o número de cartões descobertos
+                descobertas += 2;
+                // removemos o listener de eventos dos cartões descobertos
+                document.getElementById(id).removeEventListener("click", jogoMemoria);
+                document.getElementById(virado).removeEventListener("click", jogoMemoria);
+                // resetamos virado
+                virado = '';
+                if (descobertas >= 16 ){
+                    alert('Jogo encerrado');
+                }
+            } else { // Os cartões não armazenam a mesma imagem
+                // mostram o verso
+                document.getElementById(id).src = img_verso;
+                document.getElementById(virado).src = img_verso;
+                virado = '';
+            }
+        }
+    }
+}
+
+// Retorna o nome de array de forma aleatória
 function obtemImagem(imagens){
     // Quantidade de nomes de imagens contidas no array
     var num_imagens = imagens.length;
@@ -11,16 +71,11 @@ function obtemImagem(imagens){
     return img;
 }
 
-var img_verso = "verso.jpg";
 
-var imgs_cartoes = [
-    "cachorro.jpg", "cavalo.jpg", "coala.jpg",  "esquilo.jpg", 
-    "pato.jpg", "preguica.jpg",  "tigre.jpg",  "vaca.jpg",  
-    "cachorro.jpg", "cavalo.jpg", "coala.jpg", "esquilo.jpg", 
-    "pato.jpg", "preguica.jpg",  "tigre.jpg",  "vaca.jpg",              
-];
-
-var tabuleiro = [];
+/**
+ * Inicialização do jogo
+ * Definição das imagens de forma aleatória
+ */
 
 for (i=0; i < 4; i++){
     for(j=0; j<4; j++){
@@ -28,14 +83,17 @@ for (i=0; i < 4; i++){
         let id_cartao = 'cartao' + i + j;
         // Obtem o caminho da imagem
         let path_img = 'img/' + obtemImagem(imgs_cartoes);
-        console.log(id_cartao);
-        console.log(path_img);
-        // Seta o array como os paramentros do cartão
-        /*
-        tabuleiro[i][j] = {
-            'path_img' : path_img,
-        };*/
-        document.getElementById(id_cartao).src = path_img;
+
+        /**
+         * O objeto tabuleiro armazena as informações de cada cartão
+         * imagem: o caminho da imagem que aquele elemento armazena
+         */
+
+        tabuleiro[id_cartao] = { 
+            'imagem':path_img
+        };
+        document.getElementById(id_cartao).src = img_verso;
+        document.getElementById(id_cartao).addEventListener("click", jogoMemoria);
     }
 }
 
