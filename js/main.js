12653dd527cce47d00e45dd66e188d1d4caaf942
Celso Godinho da Costa <celsogod@gmail.com>
2019-04-25 18:28:36 -0300

    Lógica do jogo e estilo implementados

--- js/main.js
+++ js/main.js
@@ -1,68 +1,94 @@
-// Objeto que armazena as informações de cada cartão, indexado pelo id do cartã0
-var tabuleiro = {};
-// informa quantas imagens já foram descobertas
-var descobertas = 0;
-// armazena o id do primeiro cartão virado
-var virado = '';
-// Caminho do verso do cartão
-var img_verso = "img/verso.jpg";
+
 // Array com as imagens que podem ser usadas no jogo
-var imgs_cartoes = [
+var imgsCartoes = [
     "cachorro.jpg", "cavalo.jpg", "coala.jpg",  "esquilo.jpg", 
     "pato.jpg", "preguica.jpg",  "tigre.jpg",  "vaca.jpg",  
     "cachorro.jpg", "cavalo.jpg", "coala.jpg", "esquilo.jpg", 
     "pato.jpg", "preguica.jpg",  "tigre.jpg",  "vaca.jpg",              
 ];
 
-// Função que executa a lógica do jogo
-function jogoMemoria(){
-    let id = this.id;
-    let cartao = tabuleiro[id];
+// Objeto que armazena as informações do jogo e de cada cartão
+var jogo = {
+    'descobertas':0,// informa quantas imagens já foram descobertas
+    'idPrimCartao':'',// id do primeiro cartão  virado
+    'idSegCartao':'',// id segundo cartão virado
+    'imagemVerso': "img/verso.jpg",// Caminho do verso do cartão
+    'cartoesViradosCoincidem': false,
+    'terminou':function(){
+        if(this.descobertas == 16){
+            return true;
+        } else {
+            return false;
+        }
+    },
+};
 
-    if (virado == ''){ // Não há nenhum cartão virado
+// Verifica se os cartoes coincidem
+function verificaCoincidencia(){
+
+    document.getElementById(jogo.idPrimCartao).classList.remove("animacao");
+    document.getElementById(jogo.idSegCartao).classList.remove("animacao");
+
+    jogo.cartoesViradosCoincidem = jogo[jogo.idPrimCartao] == jogo[jogo.idSegCartao];
+
+    if (jogo.cartoesViradosCoincidem){
+        jogo.descobertas += 2;
+        // removemos o listener de eventos dos cartões descobertos
+        document.getElementById(jogo.idPrimCartao).removeEventListener("click", jogar);
+        document.getElementById(jogo.idSegCartao).removeEventListener("click", jogar);
+        jogo.cartoesViradosCoincidem = false;
+    } else { // Os cartões não armazenam a mesma imagem
+        // mostrar o verso dos cartões virados (desvirar cartões)
+        document.getElementById(jogo.idPrimCartao).src = jogo.imagemVerso;
+        document.getElementById(jogo.idSegCartao).src = jogo.imagemVerso;
+    }
+
+    // resetamos o id dos cartões virados
+    jogo.idPrimCartao = '';
+    jogo.idSegCartao = '';
+    
+}
+
+
+// Executa a lógica do jogo
+function jogar(){
+    let idCartao = this.id;
+
+    if (jogo.idPrimCartao == ''){ // Não há nenhum cartão virado
         // guarda o id do cartão clicado
-        virado = id;
+        jogo.idPrimCartao = idCartao;
         // mostra a imagem do cartão
-        this.src = cartao.imagem; 
+        this.src = jogo[idCartao]; 
     } else { // Há um cartão virado
         // Verifica se o cartão anteriormente virado é o clicado agora
-        if(virado == id){
-            // se foi o mesmo cartão clicado
+        if(jogo.idPrimCartao == idCartao){// cartão virado foi clicado
             // mostramos a imagem de verso e resetamos o virado
-            this.src = img_verso;
-            virado = '';
+            this.src = jogo.imagemVerso;
+            jogo.idPrimCartao = '';
         } else { // Um outro cartão foi clicado
             // mostramos a imagem
-            this.src = cartao.imagem; 
+            this.src = jogo[idCartao]; 
+            // seta valor do segundo cartão virado
+            jogo.idSegCartao = idCartao;
 
-            // os dois cartões armazenam a mesma imagem
-            if (tabuleiro[id].imagem == tabuleiro[virado].imagem){
-                // incrementamos o número de cartões descobertos
-                descobertas += 2;
-                // removemos o listener de eventos dos cartões descobertos
-                document.getElementById(id).removeEventListener("click", jogoMemoria);
-                document.getElementById(virado).removeEventListener("click", jogoMemoria);
-                // resetamos virado
-                virado = '';
-                if (descobertas >= 16 ){
-                    alert('Jogo encerrado');
-                }
-            } else { // Os cartões não armazenam a mesma imagem
-                // mostram o verso
-                document.getElementById(id).src = img_verso;
-                document.getElementById(virado).src = img_verso;
-                virado = '';
-            }
+            // anima os cartões virados por meio de uma classe css
+            document.getElementById(jogo.idPrimCartao).classList.add("animacao");
+            document.getElementById(jogo.idSegCartao).classList.add("animacao");
+
+            setTimeout(verificaCoincidencia, 1500);
+            
         }
     }
 }
 
+
+
 // Retorna o nome de array de forma aleatória
 function obtemImagem(imagens){
     // Quantidade de nomes de imagens contidas no array
-    var num_imagens = imagens.length;
+    var numImagens = imagens.length;
     // Obtem uma posição aleatória no array de imagens
-    var pos = Math.floor(Math.random() * num_imagens);    
+    var pos = Math.floor(Math.random() * numImagens);    
     // Guarda a imagem a ser retornada
     var img = imagens[pos];
     // Remove o nome da imagem do array
@@ -80,20 +106,18 @@ function obtemImagem(imagens){
 for (i=0; i < 4; i++){
     for(j=0; j<4; j++){
         // Monta o id da img do cartao
-        let id_cartao = 'cartao' + i + j;
+        let idCartao = 'cartao' + i + j;
         // Obtem o caminho da imagem
-        let path_img = 'img/' + obtemImagem(imgs_cartoes);
+        let pathImg = 'img/' + obtemImagem(imgsCartoes);
 
         /**
-         * O objeto tabuleiro armazena as informações de cada cartão
-         * imagem: o caminho da imagem que aquele elemento armazena
+         * O objeto jogo terá um atributo igual ao id de cada imagem cujo 
+         * valor é o caminho da imagem
          */
 
-        tabuleiro[id_cartao] = { 
-            'imagem':path_img
-        };
-        document.getElementById(id_cartao).src = img_verso;
-        document.getElementById(id_cartao).addEventListener("click", jogoMemoria);
+        jogo[idCartao] = pathImg;
+        document.getElementById(idCartao).src = jogo.imagemVerso;
+        document.getElementById(idCartao).addEventListener("click", jogar);
     }
 }
 
